7aa4b9af7077474fd66840c4093dbc282ed0404d
ismaeleBo <69010401+ismaeleBo@users.noreply.github.com>
2024-06-28 16:29:45 +0200

    fix: use https in timezonedb url

--- src/api/constants.ts
+++ src/api/constants.ts
@@ -5,4 +5,4 @@ export const GET_WEATHER_URL = "https://api.open-meteo.com/v1" as const;
 
 export const TIMEZONEDB_APIKEY = process.env.REACT_APP_TIMEZONEDB_API_KEY;
 
-export const GET_LOCATION_TIME_URL = `http://api.timezonedb.com/v2.1` as const;
+export const GET_LOCATION_TIME_URL = `https://api.timezonedb.com/v2.1` as const;
